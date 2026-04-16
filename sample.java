12f0b70ed620f331387a712d7d5f154dbe3a2468
code2116 <code2109052116@gmail.com>
2026-04-16 10:51:39 +0530

    second commit

--- sample.java
+++ sample.java
@@ -3,4 +3,12 @@ public class sample {
     public static void main(String [] args){
         System.out.println("hellow world heheh");
     }
+
+    public static void even(){
+        int n=5
+        if (n/2==0)
+        System.out.println("even");
+        else
+            System.out.println("ood");
+    }
 }
